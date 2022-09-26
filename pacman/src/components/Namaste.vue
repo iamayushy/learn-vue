3182243ee9f6a4a073e5d4b2574d6798fe7dd7de
iamayuhsy <ayushhuns@gmail.com>
2022-09-26 17:50:28 +0530

    lifecycle hook implementation

---
+++ pacman/src/components/Namaste.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+       props:{
+        user: String,
+        year: Number
+       }
+    }
+</script>
+<template>
+<h1>Namaste Dev</h1>
+<h1>{{user}} - {{year}}</h1>
+</template>
+
+<style>
+
+</style>
